873787406bf4717c48d4362747f78f9b01daae25
jameskrw <kangruiwang.cs@gmail.com>
2026-05-28 00:19:43 -0500

    minor

--- scripts/download_scannet.sh
+++ scripts/download_scannet.sh
@@ -1,6 +1,6 @@
 : "${VIEWSUITE_ROOT:?set up VIEWSUITE_ROOT first (default: your repo dir), e.g. export VIEWSUITE_ROOT=/path/to/ViewSuite}"
 
 python -m view_suite.utils.download_targz_hf \
-    --repo=ViewSuite/viewsuite \
+    --repo=MLL-Lab/viewsuite \
     --files=scannet.tar.gz \
     --out="$VIEWSUITE_ROOT/data"
